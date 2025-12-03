31a434aff61555232773350aea8dec021c8168a7
imeepos <imeepos@sker.com>
2025-12-03 11:23:00 +0800

    fix: bug

---
+++ apps/app/app/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View, Image } from 'react-native';
+import "./global.css"
+export default function HomeScreen() {
+  return (
+    <View className='text-slate-100 rounded-xl'>
+      <Text >hello</Text>
+      <Image className='p-10 w-8 h-8'/>
+    </View>
+  );
+}
