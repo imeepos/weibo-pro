0e7552dedb380a0550ef05a266a0d6b19972e121
imeepos <imeepos@sker.com>
2025-12-04 17:50:29 +0800

    fix: bug

--- apps/app/components/icon/noNews.tsx
+++ apps/app/components/icon/noNews.tsx
@@ -1,8 +1,7 @@
-export const NoNewsIcon = ({ className }: { className?: string }) => {
-    return (
-        <svg width="56" height="56" viewBox="0 0 56 56" fill="none" xmlns="http://www.w3.org/2000/svg" className={className}>
-            <path d="M0 56.0001H56V7.77245e-05H0V56.0001Z" fill="white" />
-        </svg>
+import Svg, { Path } from 'react-native-svg';
 
-    );
-};
+export const NoNewsIcon = () => (
+  <Svg width="56" height="56" viewBox="0 0 56 56" fill="none">
+    <Path d="M0 56.0001H56V7.77245e-05H0V56.0001Z" fill="white" />
+  </Svg>
+);
