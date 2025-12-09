16e61caa416464bde2f43fa67f636aba846bf7d1
imeepos <imeepos@sker.com>
2025-12-09 09:47:26 +0800

    fix: bug

--- apps/ai/src/main.ts
+++ apps/ai/src/main.ts
@@ -4,20 +4,49 @@ import { serve } from '@hono/node-server'
 
 const app = new Hono()
 
-app.post('/v1/messages', async (c) => {
-  const apiKey = `82956e56a119d60063c3cab701c7ba74.bxEZzx9O5jI9Egwe`
-  if (!apiKey) {
-    return c.json({ error: '缺少 API 密钥配置' }, 500)
-  }
-  const body = await c.req.json()
-  const headers = new Headers(c.req.header())
-  headers.set('Authorization', apiKey)
+const bigmodel = async (header: any, body: any) => {
+  // 模型负载均衡
+  const headers = new Headers(header)
+  headers.set('Authorization', `Bearer 82956e56a119d60063c3cab701c7ba74.bxEZzx9O5jI9Egwe`)
   const response = await fetch('https://open.bigmodel.cn/api/anthropic/v1/messages', {
     method: 'POST',
     headers,
     body: JSON.stringify(body)
   })
   return response;
+}
+
+const minimax = async (header: any, body: any) => {
+  // 模型负载均衡
+  const headers = new Headers(header)
+  headers.set('Authorization', `Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJHcm91cE5hbWUiOiLmnajmmI7mmI4iLCJVc2VyTmFtZSI6IuadqOaYjuaYjiIsIkFjY291bnQiOiIiLCJTdWJqZWN0SUQiOiIxOTY3NDc0OTI5OTU5OTY5MDQ2IiwiUGhvbmUiOiIiLCJHcm91cElEIjoiMTk2NzQ3NDkyOTk1MTU3NjM0MiIsIlBhZ2VOYW1lIjoiIiwiTWFpbCI6InltaW5nbWluZzM3NUBnbWFpbC5jb20iLCJDcmVhdGVUaW1lIjoiMjAyNS0xMC0zMSAxMDoxNjoxOSIsIlRva2VuVHlwZSI6MSwiaXNzIjoibWluaW1heCJ9.Ub6N1CCACkwGQjyrkh6lMYKJOsUPo1w4QtOErBZfJoi71VhAT4tJsOtw8lF0EBlLowE7eeUWNGzZYZ-Vfo4ho14wlxv9FJylOVo48CgYPNnKvKdQBCWENTyd_vcQp7k_zKs4PK-PMKka6hbG2wCp7V_4aOmZeD25VpRbRf9cFU8hzEZCCwMfztDgd-hXY6i1gR0vCvz8aot6EmriN-6vHWms2FpVAtaBveaHrmNIKM3A2glFE0gh-6jMQszfWcVz6s7Gi5qgJ7uM3y6EJ4IfOcglAPM_et9Fwlqz-OQwZhg-mDWPPZWXL0NUQjFvV6h6Q2SxAmjL-6OLaRZrfR6wDw`)
+  const response = await fetch('https://api.minimax.io/anthropic/v1/messages', {
+    method: 'POST',
+    headers,
+    body: JSON.stringify(body)
+  });
+  return response;
+}
+
+const cc12137207134 = async (header: any, body: any) => {
+  // 模型负载均衡
+  const headers = new Headers(header)
+  headers.set('Authorization', `Bearer sk-A6DtbqEHYT7cGp2psCuUVywkij0kN2mPcuTTy3CGfLDDso0y`)
+  const response = await fetch('http://121.37.207.134:8000/v1/messages', {
+    method: 'POST',
+    headers,
+    body: JSON.stringify(body)
+  });
+  return response;
+}
+
+app.post('/v1/messages', async (c) => {
+  const body = await c.req.json()
+  const { messages } = body;
+  if (messages && messages.length > 5) {
+    body.messages = messages.slice(-5)
+  }
+  return cc12137207134(c.req.header(), body)
 })
 
 // 错误处理
