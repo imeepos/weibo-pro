302cdca6c243fa98051a6a8aefb8d8d5d035f33a
imeepos <imeepos@outlook.com>
2026-01-21 21:32:46 +0800

    remove hash tree

--- packages/hashtree/src/index.ts
+++
@@ -1,4 +0,0 @@
-export { HashTree, HashTreeNode, path } from './hashtree.js'
-export { MemorySystem } from './memory.js'
-export { FileWatcher } from './watcher.js'
-export type { Hash, HashTreeOptions, MemoryIndex, Path, WatchEvent, WatchOptions, HashTreeChangeEvent } from './types.js'
