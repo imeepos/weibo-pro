4a3d7c4fe574e0dc636bfa7c6f4f784908a1624a
imeepos <imeepos@sker.com>
2026-01-22 19:25:12 +0800

    fix: bug

--- packages/workflow-run/src/WeiboKeywordSearchAstVisitor.ts
+++ packages/workflow-run/src/WeiboKeywordSearchAstVisitor.ts
@@ -17,8 +17,8 @@ import {
     EventEntity,
 } from "@sker/entities";
 import dayjs from "dayjs";
-import utc from "dayjs/plugin/utc";
-import timezone from "dayjs/plugin/timezone";
+import utc from "dayjs/plugin/utc.js";
+import timezone from "dayjs/plugin/timezone.js";
 
 dayjs.extend(utc);
 dayjs.extend(timezone);
